82c525fee1120b95838d2c5478451a89afd12eb7
zfx1101804091 <1875586562@qq.com>
2020-03-19 19:06:29 +0800

    常用配置，别名，驼峰等

--- src/main/java/com/zfx/mp/mapper/TbUserMapper.java
+++ src/main/java/com/zfx/mp/mapper/TbUserMapper.java
@@ -3,7 +3,6 @@ package com.zfx.mp.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zfx.mp.entity.TbUser;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
@@ -15,7 +14,6 @@ import java.util.List;
  * @author zheng_fx
  * @since 2020-01-18
  */
-@Mapper
 public interface TbUserMapper extends BaseMapper<TbUser> {
 
     List<TbUser> findALL();
